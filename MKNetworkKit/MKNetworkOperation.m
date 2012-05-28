bc0a5922775a1be85cd126bed02be41672fdfc2b
Nicolas Socheleau <nicolas@technick-consultant.com>
2012-05-28 22:12:28 +0200

    override http method on addFile if not post or put

--- MKNetworkKit/MKNetworkOperation.m
+++ MKNetworkKit/MKNetworkOperation.m
@@ -671,8 +671,9 @@
 }
 
 -(void) addData:(NSData*) data forKey:(NSString*) key mimeType:(NSString*) mimeType fileName:(NSString*) fileName {
-  
-  [self.request setHTTPMethod:@"POST"];
+    if (![self.request.HTTPMethod isEqualToString:@"POST"] && ![self.request.HTTPMethod isEqualToString:@"PUT"]){
+        [self.request setHTTPMethod:@"POST"];
+    }
   
   NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
                         data, @"data",
@@ -691,7 +692,10 @@
 
 -(void) addFile:(NSString*) filePath forKey:(NSString*) key mimeType:(NSString*) mimeType {
   
-  [self.request setHTTPMethod:@"POST"];
+    if (![self.request.HTTPMethod isEqualToString:@"POST"] && ![self.request.HTTPMethod isEqualToString:@"PUT"]){
+        [self.request setHTTPMethod:@"POST"];
+    }
+
   
   NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
                         filePath, @"filepath",
